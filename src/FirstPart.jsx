37207e89058186942ee807aeb1662f3c572d2e68
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-07 11:04:41 +0300

    fallback

--- src/FirstPart.jsx
+++ src/FirstPart.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useIsMounted from "react-is-mounted-hook";
+import uniqid from "uniqid";
 
 function FirstPart() {
+  const isMounted = useIsMounted();
   //give an initial state so that the data won't be undefined
-  const [bids, setBids] = useState({ usd: "usd", btc: "btc" });
+  const [asks, setAsks] = useState([0]);
+  const [bids, setBids] = useState([0]);
+  //define the number of items
+  const n = 15;
 
   const ws = new WebSocket("wss://ws.bitstamp.net");
 
@@ -23,20 +29,33 @@ function FirstPart() {
         // console.log(json.data.bids);
         // console.log(json.data.asks);
         // console.log(json.data.bids[0][0]);
-        // setData(json.data.bids[0]);
-        setBids({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
-        // setData(json.data.bids[1]);
+        // setBids({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
+        // setAsks({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
+        // console.log(json.data.bids);
+        setBids(json.data.bids);
+        setAsks(json.data.asks);
       }
     } catch (err) {
       console.log(err);
     }
+    // console.log(bids);
   };
+  // const firstNElements = bids.slice(0, n).map((item) => {
+  //   return <p key={uniqid()}>{item}</p>;
+  // });
+  // console.log(bids);
+  // const elements = () =>
+  //   bids.slice(0, n).map((item) => {
+  //     console.log(item);
+  //   });
+  // elements();
   return (
     <div>
       <p>of</p>
 
-      <p>{bids.btc}</p>
-      <p>{bids.usd} </p>
+      {/* <p>{bids.btc}</p>
+      <p>{bids.usd} </p> */}
+      {/* {firstNElements} */}
     </div>
   );
 }
