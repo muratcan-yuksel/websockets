4df4b76783411812b452ce7ed7d32d29c437b00d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-07 12:33:20 +0300

    solved the problem with the map method

--- src/FirstPart.jsx
+++ src/FirstPart.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import useIsMounted from "react-is-mounted-hook";
 import uniqid from "uniqid";
 
 function FirstPart() {
-  const isMounted = useIsMounted();
   //give an initial state so that the data won't be undefined
   const [asks, setAsks] = useState([0]);
   const [bids, setBids] = useState([0]);
@@ -21,41 +19,44 @@ function FirstPart() {
     // console.log("ororo");
     ws.send(JSON.stringify(apiCall));
   };
-  ws.onmessage = function (event) {
-    const json = JSON.parse(event.data);
-    // console.log(`[message] Data received from server: ${json}`);
-    try {
-      if ((json.event = "data")) {
-        // console.log(json.data.bids);
-        // console.log(json.data.asks);
-        // console.log(json.data.bids[0][0]);
-        // setBids({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
-        // setAsks({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
-        // console.log(json.data.bids);
-        setBids(json.data.bids);
-        setAsks(json.data.asks);
+  //clean up lest memory leaks
+  useEffect(() => {
+    ws.onmessage = function (event) {
+      const json = JSON.parse(event.data);
+      // console.log(`[message] Data received from server: ${json}`);
+      try {
+        if ((json.event = "data")) {
+          // console.log(json.data.bids);
+          // console.log(json.data.asks);
+          // console.log(json.data.bids[0][0]);
+          // setBids({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
+          // setAsks({ usd: json.data.bids[0][0], btc: json.data.bids[0][1] });
+          // console.log(json.data.bids);
+
+          setBids(json.data.bids.slice(0, 15));
+          setAsks(json.data.asks.slice(0, 15));
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
-    }
-    // console.log(bids);
-  };
-  // const firstNElements = bids.slice(0, n).map((item) => {
-  //   return <p key={uniqid()}>{item}</p>;
-  // });
-  // console.log(bids);
-  // const elements = () =>
-  //   bids.slice(0, n).map((item) => {
-  //     console.log(item);
-  //   });
-  // elements();
+      // console.log(bids);
+    };
+    //clean up function
+    return () => ws.close();
+  }, []);
+
+  const firstNElements = bids.map((item) => {
+    return <p key={uniqid()}>{item}</p>;
+  });
+
+  console.log(bids);
   return (
     <div>
       <p>of</p>
 
       {/* <p>{bids.btc}</p>
       <p>{bids.usd} </p> */}
-      {/* {firstNElements} */}
+      {firstNElements}
     </div>
   );
 }
